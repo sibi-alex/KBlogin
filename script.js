56a4bca45be9cbd0ef38632435d1c9c3b71e28aa
sibi-alex <123600396+sibi-alex@users.noreply.github.com>
2025-02-10 16:27:54 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+if("serviceWorker" in navigator){
+    navigator.serviceWorker.register("sworker.js").then(registration=>{
+        console.log("Service worker registered")
+        console.log(registration);
+    }).catch(error=>{
+        console.log("Service worker error")
+        console.log(error)
+    })
+    
+}
+else {
+    alert("Service worker not working")
+}
+
+
+
+
 let timer;
 let workTime = 0;
 let running = false;
